4e62718ef1f72a0da137b2e0806cb412d83856a4
Bromles <alexandr.v.u@gmail.com>
2023-01-29 05:38:09 +0300

    update resources

--- resources/portainer-server/setup_helm_lb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-helm repo add portainer https://portainer.github.io/k8s/
-helm repo update
-
-helm install --create-namespace -n portainer portainer portainer/portainer \
-    --set service.type=LoadBalancer \
-    --set tls.force=true
-
--- resources/portainer-server/upgrade_helm_lb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-helm repo add portainer https://portainer.github.io/k8s/
-helm repo update
-
-helm upgrade -n portainer portainer portainer/portainer \
-    --set service.type=LoadBalancer \
-    --set tls.force=true
