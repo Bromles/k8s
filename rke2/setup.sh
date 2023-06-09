178e1d8a6e9c2619e9d0cfffd4466d270dc0766e
bromles <all.bromles@gmail.com>
2023-06-09 20:39:35 +0300

    update

---
+++ rke2/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ "$(id -u)" -ne 0 ]; then echo "Please run as root." >&2; exit 1; fi
+
+mkdir -p /etc/rancher
+mkdir -p /etc/rancher/rke2
+cp config.yaml /etc/rancher/rke2/config.yaml
+
+sh rke2-install.sh
