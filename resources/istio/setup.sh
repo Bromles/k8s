5f8404acb67115fe11700a0d4ba883f6d94092ac
Bromles <all.bromles@gmail.com>
2023-07-26 11:24:43 +0300

    update istio

--- resources/istio/setup.sh
+++ resources/istio/setup.sh
@@ -2,4 +2,4 @@
 
 curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.18.1 TARGET_ARCH=arm64 sh -
 cd istio-1.18.1
-KUBECONFIG=/etc/rancher/k3s/k3s.yaml ./bin/istioctl install  -f istio-cni-k8s-gateways.yaml -y
+KUBECONFIG=/etc/rancher/k3s/k3s.yaml ./bin/istioctl install  -f ../istio-cni-k8s-gateways.yaml -y
