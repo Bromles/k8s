25c766d1a8eac9309176639b081bae8c45d5aa20
Bromles <all.bromles@gmail.com>
2023-07-26 23:17:36 +0300

    update k3s config

--- k3s/setup.sh
+++ k3s/setup.sh
@@ -5,6 +5,7 @@ if [ "$(id -u)" -ne 0 ]; then echo "Please run as root." >&2; exit 1; fi
 mkdir -p /etc/rancher
 mkdir -p /etc/rancher/k3s
 cp config-cilium.yaml /etc/rancher/k3s/config.yaml
+cp registries.yaml /etc/rancher/k3s/registries.yaml
 
 sh k3s-install.sh
 
