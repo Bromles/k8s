40077cbd0ca2797c4659448483049571ee665c66
Bromles <all.bromles@gmail.com>
2023-07-26 10:23:15 +0300

    update cilium setup

--- resources/cilium/setup.sh
+++ resources/cilium/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 install_cli_cilium.sh
+install_cli_hubble.sh
 KUBECONFIG=/etc/rancher/k3s/k3s.yaml cilium install
 KUBECONFIG=/etc/rancher/k3s/k3s.yaml cilium status --wait
 KUBECONFIG=/etc/rancher/k3s/k3s.yaml cilium hubble enable
