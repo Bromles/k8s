d7dff05f075a8f7da322b7c00ca5722aac98dec7
Bromles <all.bromles@gmail.com>
2023-07-26 09:34:03 +0300

    update cilium setup

--- resources/cilium/setup.sh
+++ resources/cilium/setup.sh
@@ -3,5 +3,5 @@
 install_cli.sh
 KUBECONFIG=/etc/rancher/k3s/k3s.yaml cilium install
 KUBECONFIG=/etc/rancher/k3s/k3s.yaml cilium status --wait
-KUBECONFIG=/etc/rancher/k3s/k3s.yaml cilium connectivity test
 KUBECONFIG=/etc/rancher/k3s/k3s.yaml cilium hubble enable
+KUBECONFIG=/etc/rancher/k3s/k3s.yaml cilium connectivity test
