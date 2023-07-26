be07eed4a493582acfd2a800d027f76a5101bc99
Bromles <all.bromles@gmail.com>
2023-07-26 09:28:24 +0300

    add cilium

--- k3s/setup.sh
+++ k3s/setup.sh
@@ -4,7 +4,7 @@ if [ "$(id -u)" -ne 0 ]; then echo "Please run as root." >&2; exit 1; fi
 
 mkdir -p /etc/rancher
 mkdir -p /etc/rancher/k3s
-cp config-calico.yaml /etc/rancher/k3s/config.yaml
+cp config-cilium.yaml /etc/rancher/k3s/config.yaml
 
 sh k3s-install.sh
 
---
+++ resources/cilium/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+install_cli.sh
+KUBECONFIG=/etc/rancher/k3s/k3s.yaml cilium install
+KUBECONFIG=/etc/rancher/k3s/k3s.yaml cilium status --wait
+KUBECONFIG=/etc/rancher/k3s/k3s.yaml cilium connectivity test
